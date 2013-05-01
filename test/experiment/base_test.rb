fa42c5bc5b780adb5caf6c877ec03b845bccc6e3
Simon <simon@sandglaz.com>
2013-05-01 09:27:14 -0400

    fixed tests broken by ruby 2.0.0 using timecop

--- test/experiment/base_test.rb
+++ test/experiment/base_test.rb
@@ -108,9 +108,6 @@ class ExperimentTest < Test::Unit::TestCase
  
   def test_experiment_keeps_created_timestamp_across_definitions
     past = Date.today - 1
-    puts ""
-    puts "past:    #{past.to_time.to_i}"
-    puts "today:   #{Date.today.to_time.to_i}"
     Timecop.freeze past.to_time do
       puts "timecop: #{Time.now.to_i}"
       assert_equal past.to_time.to_i, Time.now.to_i
